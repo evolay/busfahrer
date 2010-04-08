1a79e8638463706c6cd80f26c4fcd98faec188d2
Hubert Hoelzl <info@sketchit.de>
2010-04-08 11:52:27 +0200

    htmlcxx fixing

--- Webcrawler/src/main.cpp
+++ Webcrawler/src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include "TrudyTheCrawler.h"
-#include "htmlcxx/html/ParserDom.h"
+#include "HTMLParser.h"
 
 using namespace std;
 
-int main( char** argv, char** argc)
+/*int main( char** argv, char** argc)
 {
 	
 	TrudyTheCrawler::ThreadPool _thradpool( 10 );
@@ -15,39 +15,11 @@ int main( char** argv, char** argc)
 	}
 	return 0;
 
-}
+}*/
 
-/*
 int main(char** argv, char** argc)
 {
-  //Parse some html code
-  string html = "<html><body>hey</body></html>";
-  HTML::ParserDom parser;
-  tree<HTML::Node> dom = parser.parseTree(html);
-  
-  //Print whole DOM tree
-  cout << dom << endl;
-  
-  //Dump all links in the tree
-  tree<HTML::Node>::iterator it = dom.begin();
-  tree<HTML::Node>::iterator end = dom.end();
-  for (; it != end; ++it)
-  {
-  	if (it->tagName() == "A")
-  	{
-  		it->parseAttributes();
-  		cout << it->attributes("href");
-  	}
-  }
-  
-  //Dump all text of the document
-  it = dom.begin();
-  end = dom.end();
-  for (; it != end; ++it)
-  {
-  	if ((!it->isTag()) && (!it->isComment()))
-  	{
-  		cout << it->text();
-  	}
-  }
-}*/
+	HTMLParser var;
+	//var.parseHTML("<html><a href=\"http://www.google.de\">google</a><a href='test.html'>asfd</a></html>");
+	var.parse();
+}
